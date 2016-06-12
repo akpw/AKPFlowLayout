6f5ec126d38b1a506e852868634e85fe44e769d7
Arseniy Kuznetsov <k.arseniy@gmail.com>
2016-06-12 14:05:20 +0200

    Initial Commit

---
+++ AKPFlowLayout/AKPFlowLayout.h
@@ -0,0 +1,19 @@
+//
+//  AKPFlowLayout.h
+//  AKPFlowLayout
+//
+//  Created by Arseniy on 12/6/16.
+//  Copyright © 2016 Arseniy Kuznetsov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AKPFlowLayout.
+FOUNDATION_EXPORT double AKPFlowLayoutVersionNumber;
+
+//! Project version string for AKPFlowLayout.
+FOUNDATION_EXPORT const unsigned char AKPFlowLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AKPFlowLayout/PublicHeader.h>
+
+
