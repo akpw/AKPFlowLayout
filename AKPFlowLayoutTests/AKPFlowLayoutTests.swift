05e513820d83e32cefcdcd16132fc204d281e8ff
Arseniy Kuznetsov <k.arseniy@gmail.com>
2016-10-20 09:11:28 +0200

    swift 3 migration, tests

--- AKPFlowLayoutTests/AKPFlowLayoutTests.swift
+++ AKPFlowLayoutTests/AKPFlowLayoutTests.swift
@@ -6,44 +6,42 @@
 //  Copyright © 2016 Arseniy Kuznetsov. All rights reserved.
 //
 
-// Disabled till Quick / Nimble are converted to Swift 3...
+import Quick
+import Nimble
+import AKPFlowLayout
 
-//import Quick
-//import Nimble
-//import AKPFlowLayout
-//
-//class AKPFlowLayoutTests: QuickSpec {
-//    override func spec() {
-//        describe("AKPFlowLayout") {
-//            let akpFlowLayout: AKPFlowLayout = {
-//                $0.firsSectionMaximumStretchHeight = 200
-//                return $0
-//            }( AKPFlowLayout() )
-//            
-//            it("uses AKPFlowLayoutAttributes when creating layout attributes") {
-//                expect(AKPFlowLayout.layoutAttributesClass() == AKPFlowLayoutAttributes.self).to(beTrue())
-//            }
-//            
-//            it("has all layout config options on by default") {
-//                expect(akpFlowLayout.layoutOptions.contains(.FirstSectionIsGlobalHeader)).to(beTrue())
-//                expect(akpFlowLayout.layoutOptions.contains(.FirstSectionStretchable)).to(beTrue())
-//                expect(akpFlowLayout.layoutOptions.contains(.SectionsPinToGlobalHeaderOrVisibleBounds)).to(beTrue())
-//            }
-//            
-//            it("when running on iSO9, it disables built-in sectionHeadersPinToVisibleBounds") {
-//                if #available(iOS 9.0, *) {
-//                    akpFlowLayout.sectionHeadersPinToVisibleBounds = false
-//                    expect(akpFlowLayout.sectionHeadersPinToVisibleBounds).to(beFalse())
-//
-//                    akpFlowLayout.sectionHeadersPinToVisibleBounds = true
-//                    expect(akpFlowLayout.sectionHeadersPinToVisibleBounds).to(beFalse())
-//                }
-//            }
-//            
-//            it("has given firsSectionMaximumStretchHeight value") {
-//                expect(akpFlowLayout.firsSectionMaximumStretchHeight == 200).to(beTrue())
-//            }
-//            
-//        }
-//    }
-//}
+class AKPFlowLayoutTests: QuickSpec {
+    override func spec() {
+        describe("AKPFlowLayout") {
+            let akpFlowLayout: AKPFlowLayout = {
+                $0.firsSectionMaximumStretchHeight = 200
+                return $0
+            }( AKPFlowLayout() )
+            
+            it("uses AKPFlowLayoutAttributes when creating layout attributes") {
+                expect(AKPFlowLayout.layoutAttributesClass() == AKPFlowLayoutAttributes.self).to(beTrue())
+            }
+            
+            it("has all layout config options on by default") {
+                expect(akpFlowLayout.layoutOptions.contains(.FirstSectionIsGlobalHeader)).to(beTrue())
+                expect(akpFlowLayout.layoutOptions.contains(.FirstSectionStretchable)).to(beTrue())
+                expect(akpFlowLayout.layoutOptions.contains(.SectionsPinToGlobalHeaderOrVisibleBounds)).to(beTrue())
+            }
+            
+            it("when running on iSO9, it disables built-in sectionHeadersPinToVisibleBounds") {
+                if #available(iOS 9.0, *) {
+                    akpFlowLayout.sectionHeadersPinToVisibleBounds = false
+                    expect(akpFlowLayout.sectionHeadersPinToVisibleBounds).to(beFalse())
+
+                    akpFlowLayout.sectionHeadersPinToVisibleBounds = true
+                    expect(akpFlowLayout.sectionHeadersPinToVisibleBounds).to(beFalse())
+                }
+            }
+            
+            it("has given firsSectionMaximumStretchHeight value") {
+                expect(akpFlowLayout.firsSectionMaximumStretchHeight == 200).to(beTrue())
+            }
+            
+        }
+    }
+}
